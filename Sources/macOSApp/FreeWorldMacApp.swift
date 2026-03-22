5c1f38f21eb606819ed20309dae463f89fe48dce
egor <egor802969@icloud.com>
2026-03-22 21:40:13 +0300

    Исправлен показ окна macOS

--- Sources/macOSApp/FreeWorldMacApp.swift
+++ Sources/macOSApp/FreeWorldMacApp.swift
@@ -1,11 +1,39 @@
 import SwiftUI
 #if os(macOS)
 import AppKit
+
+final class FreeWorldAppDelegate: NSObject, NSApplicationDelegate {
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        NSApp.setActivationPolicy(.regular)
+        NSApp.activate(ignoringOtherApps: true)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.12) {
+            self.bringWindowToFront()
+        }
+    }
+
+    func applicationShouldHandleReopen(_ sender: NSApplication, hasVisibleWindows flag: Bool) -> Bool {
+        sender.setActivationPolicy(.regular)
+        sender.activate(ignoringOtherApps: true)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.05) {
+            self.bringWindowToFront()
+        }
+        return true
+    }
+
+    private func bringWindowToFront() {
+        guard let window = NSApp.windows.first else { return }
+        window.orderFrontRegardless()
+        window.makeKeyAndOrderFront(nil)
+    }
+}
 #endif
 
 @main
 struct FreeWorldMacApp: App {
     @StateObject private var viewModel: GameViewModel
+    #if os(macOS)
+    @NSApplicationDelegateAdaptor(FreeWorldAppDelegate.self) private var appDelegate
+    #endif
 
     @MainActor
     init() {
